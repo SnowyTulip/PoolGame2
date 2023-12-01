a0d70a0e1d4eeb5b8238fa1442ff25570bd83f4e
博 高 <2739467614@qq.com>
2023-12-01 14:48:00 +0800

    初始化提交

---
+++ src/main/java/PoolGame/StartProxy.java
@@ -0,0 +1,15 @@
+package PoolGame;
+
+/**
+ * @projectName: Design_Pattern_Assignment_2
+ * @package: PoolGame
+ * @className: StartProxy
+ * @author: pi
+ * @description: Make a start proxy for App.main()
+ * @version: 1.0
+ */
+public class StartProxy {
+    public static void main(String[] args){
+        App.main(args);
+    }
+}
